89c172f33df3c28940201d9dc3f40a2a1e2cc856
shameem <shameem3181@gmail.com>
2018-01-16 09:21:57 +0000

    consfigured platform

--- Layout7/src/test/java/Utils/Invoke.java
+++ Layout7/src/test/java/Utils/Invoke.java
@@ -23,86 +23,78 @@ public class Invoke
     public static final String CHROME="chrome";
     public static final String FIREFOX="firefox";
     public static final String IE="InternetExplorer";
+    public EventFiringWebDriver webdriver;
+@Before
 
+    public static void setDriver() {
 
-    public static void setDriver()
-        {
+    if (webDriver == null) {
+        if (AutomationConstants.BROWSER.equals("chrome")) {
+            LOG.info("Starting the Browser");
 
-            if (webDriver == null)
-            {
-                if(AutomationConstants.BROWSER.equals("chrome"))
-                {
-                   LOG.info("Starting the Browser");
+            setDriverPathForBrowser("CHROME");
 
-                   setDriverPathForBrowser("CHROME");
-
-                        if(isLinux())
-                        {
-                            ChromeOptions options = new ChromeOptions();
-                            //options.setBinary(userpath + "/src/test/resources/drivers/Ubuntu/google-chrome-stable_current_amd64.deb");
-                            options.setBinary(userpath + "C:\\Users\\akhta\\Desktop\\opencart-layout7\\Layout7\\src\\test\\Resources\\drivers\\ubuntu\\chromedriver.exe");
-                            webDriver = new EventFiringWebDriver(new ChromeDriver(options));
-                            webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-
-                        }
-                        else
-
-                        {
-                            webDriver = new EventFiringWebDriver(new ChromeDriver());
-                        }
-
-                    webDriver.manage().window().maximize();
-                    }
-
-                }
-                else if (FIREFOX.equals("yes"))
-                {
-
-                    setDriverPathForBrowser("Firefox");
-                    // set binary path??
-                    webDriver = new EventFiringWebDriver(new FirefoxDriver());
-                    webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-                    webDriver.manage().deleteAllCookies();
-                    webDriver.manage().window().fullscreen();
-
-                }
-                else if (IE.equals("yes"))
-                {
-
-                    System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
-                    webDriver = new EventFiringWebDriver(new InternetExplorerDriver());
-
-                    webDriver.manage().window().fullscreen();
-                    webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-                    webDriver.manage().deleteAllCookies();
-
-                }
-                else {
-                   LOG.info("No Driver Initialised");
-                }
-            webDriver.get(AutomationConstants.URL);
+            if (isLinux()) {
+                ChromeOptions options = new ChromeOptions();
+                options.setBinary(userpath + "/src/test/resources/drivers/Ubuntu/google-chrome-stable_current_amd64.deb");
+                webDriver = new EventFiringWebDriver(new ChromeDriver(options));
+                webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
             }
+            //
+            //webDriver.manage().window().maximize();
+            //}
 
+
+//        } else if (FIREFOX.equals("yes")) {
+//
+//            setDriverPathForBrowser("Firefox");
+//            // set binary path??
+//            webDriver = new EventFiringWebDriver(new FirefoxDriver());
+//            webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+//            webDriver.manage().deleteAllCookies();
+//            webDriver.manage().window().fullscreen();
+//
+//        } else if (IE.equals("yes")) {
+//
+//            System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
+//            webDriver = new EventFiringWebDriver(new InternetExplorerDriver());
+//
+//            webDriver.manage().window().fullscreen();
+//            webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+//            webDriver.manage().deleteAllCookies();
+//
+//        } else {
+//            LOG.info("No Driver Initialised");
+//        }
+//    }
+        }
+
+            webDriver = new EventFiringWebDriver(new ChromeDriver());
+            webDriver.get(AutomationConstants.URL);
+
+    }
+}
 
     private static void setDriverPathForBrowser(String browserName) {
         {
            browserName = browserName.toLowerCase();
-            if (browserName.equals("CHROME")) {
+            if (browserName.equalsIgnoreCase("CHROME")) {
                 if (isMac()) {
-                    System.setProperty("webdriver.chrome.driver", userpath + "/src/test/resources/drivers/mac/chromedriver");
+                    System.setProperty("webdriver.chrome.driver",userpath+"/src/test/resources/drivers/mac/chromedriver");
                 } else if (isLinux()) {
-                    System.setProperty("webdriver.chrome.driver", userpath + "/src/test/resources/drivers/ubuntu/chromedriver");
-                } else if (isWindows()) {
-                    System.setProperty("webdriver.chrome.driver", userpath + "drivers/windows/chromedriver.exe");
+                    System.setProperty("webdriver.chrome.driver",userpath+"/src/test/resources/drivers/ubuntu/chromedriver");
+                } else  {
+
+                    System.setProperty("webdriver.chrome.driver",userpath+"/src/test/resources/drivers/windows/chromedriver.exe");
                 }
-            } else if (browserName.equals("Firefox")) {
+            } else if (browserName.equalsIgnoreCase("Firefox")) {
                 if (isMac()) {
-                    System.setProperty("webdriver.gecko.driver", userpath + "/src/test/resources/drivers/mac/chromedriver");
+                    System.setProperty("webdriver.gecko.driver",userpath+"/src/test/resources/drivers/mac/chromedriver");
                 } else if (isLinux()) {
-                    System.setProperty("webdriver.gecko.driver", userpath + "/src/test/resources/drivers/ubuntu/chromedriver");
+                    System.setProperty("webdriver.gecko.driver",userpath+"/src/test/resources/drivers/ubuntu/chromedriver");
                 } else if (isWindows()) {
-                    System.setProperty("webdriver.gecko.driver", userpath + "/src/test/resources/drivers/windows/chromedriver.exe");
+                    System.setProperty("webdriver.gecko.driver",userpath+"/src/test/resources/drivers/windows/chromedriver.exe");
                 }
             }
 
@@ -110,11 +102,7 @@ public class Invoke
     }
 
 
-    public static EventFiringWebDriver getDriver()
-    {
-        setDriver();
-        return webDriver;
-    }
+
     private static boolean isWindows() {
         return (OS.indexOf("windows") >= 0);
     }
@@ -128,9 +116,9 @@ public class Invoke
     }
 
 
+    }
 
 
-}
 
 
 
